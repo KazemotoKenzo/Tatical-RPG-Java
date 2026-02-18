d3f8fb84cf309e414f64aa368c9c40ae49fa60da
Kazemoto <kenzo80804@gmail.com>
2026-02-18 18:20:36 -0300

    first commit

---
+++ src/main/java/com/kz/tatical_rpg/TaticalRpgApplication.java
@@ -0,0 +1,13 @@
+package com.kz.tatical_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaticalRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaticalRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kz/tatical_rpg/TaticalRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kz.tatical_rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaticalRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
