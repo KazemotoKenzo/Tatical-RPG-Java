bbb890867c4a0a07f46df2c1c883f419bf61dbd1
Kazemoto <kenzo80804@gmail.com>
2026-02-18 23:43:05 -0300

    adding input reader

--- src/main/java/com/kz/tatical_rpg/Services/CombatService.java
+++ src/main/java/com/kz/tatical_rpg/Services/CombatService.java
@@ -6,47 +6,68 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 @Service
 public class CombatService {
     private ArrayList<Entity> sequencelist = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    private int enemies_hp = 0;
+    int select;
 
     @PostConstruct
     public void mainRunner(){
         createEntityTest();
+        selectInput();
+    }
+
+    private void selectInput(){
+        while (true){
+            try{
+                for(Entity i : sequencelist){ System.out.println("\n========================\n" + i + "\n========================\n"); }
+                System.out.println("Select a entity to deal damage: ");
+                select = scanner.nextInt();
+
+                if(select >= 0 && select < sequencelist.toArray().length){
+                    break;
+                }   else { System.out.println("Error: Try selecting a number between 0 and " + (sequencelist.toArray().length-1)); scanner.nextLine(); }
+
+            }   catch (InputMismatchException e) {
+                System.out.println("Error: Try to input a integer number.");
+                scanner.nextLine();
+            }
+        }
+        selectEnemy(select);
+        scanner.nextLine();
+    }
+
+    private void selectEnemy(int value){
+        int damage = 1;
+
+        Entity entity = sequencelist.get(value);
+        entity.takeDamage(damage);
+        enemies_hp -= damage;
+        System.out.println(entity.getName() + " was hurt!\n Now it has " + entity.getHp() + "/" + entity.getHp_max() + " hp.");
     }
 
     private void addSequence(Entity entity){
         sequencelist.add(entity);
+        enemies_hp += entity.getHp_max();
         sequencelist.sort(Comparator.comparingInt(Entity::getInitiative));
     }
 
     private void createEntityTest(){
-        Entity entity1 = new Entity();
-        Entity entity2 = new Entity();
-        Entity entity3 = new Entity();
-        Entity entity4 = new Entity();
-
-        entity1.setInitiative(1);
-        entity1.setName("entity 1");
-
-        entity2.setInitiative(2);
-        entity2.setName("entity 2");
-
-        entity3.setInitiative(3);
-        entity3.setName("entity 3");
-
-        entity4.setInitiative(4);
-        entity4.setName("entity 4");
-
+        Entity entity1 = new Entity("Entity 1", 1, 10, 1);
+        Entity entity2 = new Entity("Entity 2", 2, 10, 1);
+        Entity entity3 = new Entity("Entity 3", 3, 10, 1);
+        Entity entity4 = new Entity("Entity 4", 4, 10, 1);
 
         addSequence(entity4);
         addSequence(entity2);
         addSequence(entity3);
         addSequence(entity1);
 
-        for(Entity i : sequencelist){
-            System.out.println(i.getName());
-        }
+        for(Entity i : sequencelist){ System.out.println("\n========================\n" + i + "\n========================\n"); }
     }
 }
--- src/main/java/com/kz/tatical_rpg/domain/Entity.java
+++ src/main/java/com/kz/tatical_rpg/domain/Entity.java
@@ -4,6 +4,55 @@ public class Entity {
     private String name;
     private int initiative;
     private int actions;
+    private int hp;
+    private int hp_max;
+    private int damage;
+
+    public Entity(String name, int initiative, int hp_max, int damage){
+        this.name = name;
+        this.initiative = initiative;
+        this.hp_max = hp_max;
+        this.hp = this.hp_max;
+        this.damage = damage;
+    }
+
+    public Entity(){
+        this("Unknown", 0, 0, 0);
+    }
+
+    @Override
+    public String toString(){
+        return this.name + "\nHP: " + this.hp + "/" + this.hp_max;
+    }
+
+    public int takeDamage(int damage_){
+        this.hp -= damage_;
+        return damage_;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getHp_max() {
+        return hp_max;
+    }
+
+    public void setHp_max(int hp_max) {
+        this.hp_max = hp_max;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
 
     public String getName() {
         return name;
