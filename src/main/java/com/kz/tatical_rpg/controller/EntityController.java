9629ca6e11f6861fc6669bf5e099342d58d89493
Kazemoto <kenzo80804@gmail.com>
2026-02-26 22:07:47 -0300

    preparing EntityController to manager damage system from Entity

--- src/main/java/com/kz/tatical_rpg/controller/EntityController.java
+++ src/main/java/com/kz/tatical_rpg/controller/EntityController.java
@@ -1,4 +1,34 @@
 package com.kz.tatical_rpg.controller;
 
 public class EntityController {
+    /*
+    public int takeDamage(int damage_, boolean true_damage){
+        if(!true_damage && this.barrier > 0){
+            int discount = damage_ - this.barrier;
+            if (discount < 0) discount = 0;
+            this.barrier -= damage_;
+            if(this.barrier <= 0) {
+                this.barrier_max = 0;
+                this.barrier = 0;
+            }
+            damage_ = discount;
+        }
+
+        this.hp -= damage_;
+
+        if(isDead()) System.out.println("The entity has been slayed.");
+        return damage_;
+    }
+    */
+    /*
+    private boolean isDead(){
+        if(this.hp >= this.hp_max)  this.hp = this.hp_max;
+
+        if(this.hp <= 0){
+            this.hp = 0;
+            this.dead = true;
+        }
+        return this.dead;
+    }
+     */
 }
